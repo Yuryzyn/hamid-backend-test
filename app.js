3737988d34e648005b153ae0e7831697fad04632
Ahmad Yuridan Zindani <58173851+Yuryzyn@users.noreply.github.com>
2023-10-09 17:31:41 +0700

    Update app.js
    re-fix foto barang

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.hidePoweredBy({ setTo: "PHP 7.4.11" }));
 
-app.use("/fotoBarang", express.static(path.join(__dirname,'fotoBarang')));
+app.use("/fotoBarang", express.static(path.join(__dirname,'./fotoBarang')));
 
 app.use("/", router);
 
