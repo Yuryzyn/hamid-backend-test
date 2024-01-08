2965ac137629f21f55bfb4a2b8f81ff70053e0c2
Ahmad Yuridan Zindani <ayzindani@student.ittelkom-sby.ac.id>
2024-01-08 13:22:14 +0700

    switch to db demo

--- db.connect.js
+++ db.connect.js
@@ -3,8 +3,14 @@ var mongoose = require("mongoose");
 
  dotenv.config({path: "./src/.env"});
 
- const database = process.env.DATABASE;
- const folder = process.env.DB_FOLDER;
+ const database = process.env.DATABASE2;
+ let folder
+
+ if (database === process.env.DATABASE){
+  folder = process.env.DB_FOLDER;
+ } else if (database === process.env.DATABASE2){
+  folder = process.env.DB_FOLDER2;
+ }
 
 const dbConnect = () => {
   const mongoURI= database;
@@ -17,7 +23,7 @@ const dbConnect = () => {
      connectTimeoutMS: 15000,
    });
    let db = mongoose.connection;
-   db.on("error", console.log.bind(console, "connection error"));
+   db.on("error", console.log.bind(console, "database connection error"));
    db.once("open", function () {
      console.log("MONGODB CONNECTED TO : "+folder);
   });
